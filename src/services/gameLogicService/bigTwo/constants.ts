8dc50656ed0a13ccceabdba998668f9d3a51692c
Victor Cheng <victorcheng@Victors-MacBook-Air.local>
2024-04-21 12:02:03 +0800

    finish Straight constant

--- src/services/gameLogicService/bigTwo/constants.ts
+++ src/services/gameLogicService/bigTwo/constants.ts
@@ -37,7 +37,14 @@ export enum FiveCardHand {
 }
 
 export const Straight: { hand: Set<CardRank>, pivot: CardRank, rank: number }[] = [
-    { hand: new Set([CardRank.Three, CardRank.Four, CardRank.Five, CardRank.Six, CardRank.Seven]), pivot: CardRank.Five, rank: 1 },
-    { hand: new Set([CardRank.Four, CardRank.Five, CardRank.Six, CardRank.Eight]), pivot: CardRank.Six, rank: 2 },
-    // TBD
+    { hand: new Set([CardRank.Three, CardRank.Four, CardRank.Five, CardRank.Six, CardRank.Seven]), pivot: CardRank.Seven, rank: 1 },
+    { hand: new Set([CardRank.Four, CardRank.Five, CardRank.Six, CardRank.Seven, CardRank.Eight]), pivot: CardRank.Eight, rank: 2 },
+    { hand: new Set([CardRank.Five, CardRank.Six, CardRank.Seven, CardRank.Eight, CardRank.Nine]), pivot: CardRank.Nine, rank: 3 },
+    { hand: new Set([CardRank.Six, CardRank.Seven, CardRank.Eight, CardRank.Nine, CardRank.Ten]), pivot: CardRank.Ten, rank: 4 },
+    { hand: new Set([CardRank.Seven, CardRank.Eight, CardRank.Nine, CardRank.Ten, CardRank.Jack]), pivot: CardRank.Jack, rank: 5 },
+    { hand: new Set([CardRank.Eight, CardRank.Nine, CardRank.Ten, CardRank.Jack, CardRank.Queen]), pivot: CardRank.Queen, rank: 6 },
+    { hand: new Set([CardRank.Nine, CardRank.Ten, CardRank.Jack, CardRank.Queen, CardRank.King]), pivot: CardRank.King, rank: 7 },
+    { hand: new Set([CardRank.Ten, CardRank.Jack, CardRank.Queen, CardRank.King, CardRank.Ace]), pivot: CardRank.Ace, rank: 8 },
+    { hand: new Set([CardRank.Two, CardRank.Three, CardRank.Four, CardRank.Five, CardRank.Six]), pivot: CardRank.Two, rank: 9 },
+    { hand: new Set([CardRank.Ace, CardRank.Two, CardRank.Three, CardRank.Four, CardRank.Five]), pivot: CardRank.Two, rank: 10 }
 ]
