15f3cb4c062903b1c17ea4a6b84a4089d8389684
Victor Cheng <victorcheng@Victors-MacBook-Air.local>
2024-04-21 14:49:15 +0800

    write Big Two game logics to pass unit tests

--- src/services/gameLogicService/bigTwo/logics.ts
+++ src/services/gameLogicService/bigTwo/logics.ts
@@ -1,6 +1,6 @@
 import { Card } from "./types"
 import { Straights } from "./constants"
-import _ from "lodash"
+import _, { stubFalse } from "lodash"
 
 export function hasSameRank(cards: Card[]): boolean {
     return cards.every(card => card.rank === cards[0].rank)
@@ -10,12 +10,6 @@ export function hasSameSuit(cards: Card[]): boolean {
     return cards.every(card => card.suit === cards[0].suit)
 }
 
-export function sortByRanks(cards: Card[]): Card[] {
-    const cardsCopy = [...cards]
-    cardsCopy.sort((a, b) => a.rank - b.rank)
-    return cardsCopy
-}
-
 export function isPair(cards: Card[]): boolean {
     return cards.length === 2 && hasSameRank(cards)
 }
@@ -23,20 +17,41 @@ export function isTriple(cards: Card[]): boolean {
     return cards.length === 3 && hasSameRank(cards)
 }
 
-export function isFiveCardHand() {}
-
 export function isStraight(cards: Card[]): boolean {
     if (cards.length !== 5) {
         return false
     }
     const cardRanks = new Set(cards.map(card => card.rank))
-    return Straights.filter(straight => _.isEqual(straight.hand, cardRanks)).length > 0
+    return Straights.filter(straight => _.isEqual(straight.hand, cardRanks)).length > 0 && !hasSameSuit(cards)
 }
 
-export function isFlush() {}
+export function isFlush(cards: Card[]): boolean {
+    if (cards.length !== 5) {
+        return false
+    }
+    return hasSameSuit(cards) && !isStraightFlush(cards)
+}
 
-export function isFullHouse() {}
+export function isFullHouse(cards: Card[]) {
+    if (cards.length !== 5) {
+        return false
+    }
+    const cardRanks = cards.map(card => card.rank)
+    return (cardRanks.filter(cardRank => cardRank === cardRanks[0]).length === 2 || cardRanks.filter(cardRank => cardRank === cardRanks[0]).length === 3) && new Set(cardRanks).size === 2
+}
 
-export function isFourOfAKind() {}
+export function isFourOfAKind(cards: Card[]) {
+    if (cards.length !== 5) {
+        return false
+    }
+    const cardRanks = cards.map(card => card.rank)
+    return (cardRanks.filter(cardRank => cardRank === cardRanks[0]).length === 1 || cardRanks.filter(cardRank => cardRank === cardRanks[0]).length === 4) && new Set(cardRanks).size === 2
+}
 
-export function isStraightFlush() {}
+export function isStraightFlush(cards: Card[]) {
+    if (cards.length !== 5) {
+        return false
+    }
+    const cardRanks = new Set(cards.map(card => card.rank))
+    return Straights.filter(straight => _.isEqual(straight.hand, cardRanks)).length > 0 && hasSameSuit(cards)
+}
