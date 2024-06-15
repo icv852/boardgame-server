72436f5b8a62f7e738158c2bd4391e45e6ca1734
Victor Cheng <victorcheng@Victors-MacBook-Air.local>
2024-06-15 14:29:11 +0800

    WIP

---
+++ src/services/gameLogicService/bigTwo/logics.test.ts
@@ -0,0 +1,87 @@
+import * as BigTwoLogics from "./logics"
+import { Card, Player, GameState, Play, Pass, Move } from "./types"
+import { Rank, Suit, Seat } from "./constants"
+import { Option } from "effect"
+
+const SpadeA: Card = { suit: Suit.Spade, rank: Rank.Ace }
+const Spade2: Card = { suit: Suit.Spade, rank: Rank.Two }
+const Spade3: Card = { suit: Suit.Spade, rank: Rank.Three }
+const Spade4: Card = { suit: Suit.Spade, rank: Rank.Four }
+const Spade5: Card = { suit: Suit.Spade, rank: Rank.Five }
+const Spade6: Card = { suit: Suit.Spade, rank: Rank.Six }
+const Spade7: Card = { suit: Suit.Spade, rank: Rank.Seven }
+const Heart2: Card = { suit: Suit.Heart, rank: Rank.Two }
+const Heart3: Card = { suit: Suit.Heart, rank: Rank.Three }
+const Club3: Card = { suit: Suit.Club, rank: Rank.Three }
+const Diamond3: Card = { suit: Suit.Diamond, rank: Rank.Three }
+
+const playerNorth: Player = { seat: Seat.North, score: 0, hands: [SpadeA] }
+const playerWest: Player = { seat: Seat.West, score: 0, hands: [Spade2] }
+const playerSouth: Player = { seat: Seat.South, score: 0, hands: [Spade3] }
+const playerEast: Player = { seat: Seat.East, score: 0, hands: [Club3] }
+const players: Player[] = [playerNorth, playerWest, playerSouth, playerEast]
+
+const playSpade1BySouth: Move = {
+    type: "Play",
+    seat: Seat.South,
+    cards: [Spade3]
+}
+
+const playSpadeAByNorth: Move = {
+    type: "Play",
+    seat: Seat.North,
+    cards: [SpadeA]
+}
+
+const playClub3ByEast: Move = {
+    type: "Play",
+    seat: Seat.East,
+    cards: [Club3]
+}
+
+const passByEast: Pass = {
+    type: "Pass",
+    seat: Seat.East
+}
+
+const gameState1: GameState = {
+    players: players,
+    currentSeat: Seat.North,
+    leadingPlay: Option.none()
+}
+
+const gameState2: GameState = {
+    players: players,
+    currentSeat: Seat.South,
+    leadingPlay: Option.some(playClub3ByEast)
+}
+
+describe("Big Two Logic Functions", () => {
+    describe("Extracting game info from GameState and Move", () => {
+        expect(BigTwoLogics.getCurrentSeat(gameState1)).toStrictEqual(Seat.North)
+        expect(BigTwoLogics.getLeadingPlay(gameState1)).toStrictEqual(Option.none())
+        expect(BigTwoLogics.getLeadingPlay(gameState2)).toStrictEqual(Option.some(playClub3ByEast))
+        expect(BigTwoLogics.getSeatOfLeadingPlay(gameState1)).toStrictEqual(Option.none())
+        expect(BigTwoLogics.getSeatOfLeadingPlay(gameState2)).toStrictEqual(Option.some(Seat.East))
+        expect(BigTwoLogics.getCardsOfLeadingPlay(gameState1)).toStrictEqual(Option.none())
+        expect(BigTwoLogics.getCardsOfLeadingPlay(gameState2)).toStrictEqual(Option.some([Club3]))
+        expect(BigTwoLogics.getSeatOfMove(playSpade1BySouth)).toStrictEqual(Seat.South)
+        expect(BigTwoLogics.getTypeOfMove(playSpade1BySouth)).toStrictEqual("Play")
+        expect(BigTwoLogics.getCardsOfPlay(playSpade1BySouth)).toStrictEqual([Spade3])
+    })
+
+    describe("Determining facts in game", () => {
+        expect(BigTwoLogics.isCurrentSeat(gameState1)(playSpade1BySouth)).toStrictEqual(false)
+        expect(BigTwoLogics.isCurrentSeat(gameState1)(playSpadeAByNorth)).toStrictEqual(true)
+        expect(BigTwoLogics.isPass(playSpade1BySouth)).toStrictEqual(false)
+        expect(BigTwoLogics.isPass(passByEast)).toStrictEqual(true)
+        expect(BigTwoLogics.isPlay(playSpade1BySouth)).toStrictEqual(true)
+        expect(BigTwoLogics.isPlay(passByEast)).toStrictEqual(false)
+    })
+
+    describe("Check if a pass is valid", () => {
+        it("should return false if leading play not exists", () => expect(BigTwoLogics.isPassValid(gameState1)(passByEast)).toStrictEqual(false))
+        it("should return false if the leading player passes", () => expect(BigTwoLogics.isPassValid({ players: players, currentSeat: Seat.East, leadingPlay: Option.some(playClub3ByEast) })(passByEast)).toStrictEqual(false))
+        it("should return true if it's a valid pass", () => expect(BigTwoLogics.isPassValid(gameState2)({ type: "Pass", seat: Seat.South })).toStrictEqual(true))
+    })
+})
--- src/services/gameLogicService/bigTwo/logics.ts
+++ src/services/gameLogicService/bigTwo/logics.ts
@@ -7,7 +7,7 @@ logics:
 
 if current seat:
     if pass:
-        if passable: apply move
+        if valid pass: apply move
         else: error
     if play:
         if valid play:
@@ -20,12 +20,20 @@ else: error
 */
 
 
-const getCurrentSeat = (gameState: GameState): Seat => gameState.currentSeat
-const getLeadingPlay = (gameState: GameState): Option.Option<Play> => gameState.leadingPlay
-const getSeatOfMove = (move: Move): Seat => move.seat
-const getTypeOfMove = (move: Move): string => move.type
-const getCardsOfPlay = (play: Play): Card[] => play.cards
+export const getCurrentSeat = (gameState: GameState): Seat => gameState.currentSeat
+export const getLeadingPlay = (gameState: GameState): Option.Option<Play> => gameState.leadingPlay
+export const getSeatOfLeadingPlay = (gameState: GameState): Option.Option<Seat> => Option.map(getLeadingPlay(gameState), lp => lp.seat)
+export const getCardsOfLeadingPlay = (gameState: GameState): Option.Option<Card[]> => Option.map(getLeadingPlay(gameState), lp => lp.cards)
+export const getSeatOfMove = (move: Move): Seat => move.seat
+export const getTypeOfMove = (move: Move): string => move.type
+export const getCardsOfPlay = (play: Play): Card[] => play.cards
 
-const isCurrentSeat = (moveSeat: Seat) => (currentSeat: Seat): boolean => moveSeat === currentSeat
+export const isCurrentSeat = (gameState: GameState) => (move: Move) : boolean => getSeatOfMove(move) === getCurrentSeat(gameState)
 
-const isPass = (move: Move): boolean => move.type === "Pass"
+export const isPass = (move: Move): boolean => getTypeOfMove(move) === "Pass"
+export const isPlay = (move: Move): boolean => getTypeOfMove(move) === "Play"
+
+export const isPassValid = (gameState: GameState) => (move: Pass) => {
+    const leadingPlay = getLeadingPlay(gameState)
+    return Option.isSome(leadingPlay) && (Option.getOrThrow(getSeatOfLeadingPlay(gameState)) !== getSeatOfMove(move))
+}
