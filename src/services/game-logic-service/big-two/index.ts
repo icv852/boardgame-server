120e87b8e3389ba2375c6476817fccf3475ca78e
Victor Cheng <59554817+icv852@users.noreply.github.com>
2024-12-03 09:53:21 +0000

    WIP

--- src/services/game-logic-service/big-two/index.ts
+++ src/services/game-logic-service/big-two/index.ts
@@ -3,81 +3,31 @@ import { Card, GameState, Move, Pass, Play, Player, Single } from "./types";
 import { GameLogicError } from "../../../utils/errors";
 import { Seat } from "./constants";
 
-const assignCurrentToNextPlayer = (gameState: GameState): GameState =>({ ...gameState, current: Seat.getNext(gameState.current) })
-
-const isLeadExists = (gameState: GameState): boolean => Option.isSome(gameState.lead)
-
-const failIfLeadNotExists = (gameState: GameState): Effect.Effect<GameState, GameLogicError> => pipe(
-    Effect.succeed(gameState),
-    Effect.andThen(gs => isLeadExists(gs) ? Effect.succeed(gs) : Effect.fail(new GameLogicError("Can't make a pass when lead doesn't exist.")))
-)
-
-const resetLeadIfPassingToLead = (pass: Pass) => (gameState: GameState): GameState => pipe(
+const validateLeadExists = (gameState: GameState): Effect.Effect<GameState, GameLogicError> => pipe(
     gameState.lead,
-    Option.andThen(lead => Seat.getNext(pass.seat) === lead.seat),
-    Option.getOrElse(() => false),
-    isPassingToLead => isPassingToLead ? ({ ...gameState, lead: Option.none() }) : gameState
+    Option.isSome,
+    leadExists => leadExists ? Effect.succeed(gameState) : Effect.fail(new GameLogicError("Cannot pass when the lead doesn't exist."))
 )
 
-const getPlayerBySeat = (seat: Seat) => (gameState: GameState): Player => gameState.players.find(player => player.seat === seat)
+const mutateUpdate
 
-const isNextPlayerLastCard = (gameState: GameState): boolean => getPlayerBySeat(Seat.getNext(gameState.current))(gameState).hands.length === 1
-
-const isLeadSingle = (gameState: GameState): boolean => Option.match(gameState.lead, { onSome: lead => lead.value instanceof Single, onNone: () => false })
-
-const checkIfPassSuspectAssistance = (pass: Pass) => (gameState: GameState): GameState => pipe(
-    isNextPlayerLastCard(gameState),
-    isNextPlayerLastCard => isNextPlayerLastCard ? Option.some(gameState) : Option.none(),
-    Option.andThen(gs => isLeadSingle(gs) ? Option.some(gs) : Option.none()),
-    Option.andThen(gs => {
-        const leadCard = Option.getOrThrow(gs.lead).cards[0]
-        return getPlayerBySeat(pass.seat)(gameState).hands.some(hand => hand.canBeat(leadCard)) ? Option.some(gs) : Option.none()
-    }),
-    Option.andThen(gs => ({ ...gs, suspectedAssistance: true })),
-    Option.getOrElse(() => ({ ...gameState, suspectedAssistance: false }))
-)
-
-const checkIfPlaySuspectAssistance = (play: Play) => (gameState: GameState): GameState => pipe(
-    isNextPlayerLastCard(gameState),
-    isNextPlayerLastCard => isNextPlayerLastCard ? Option.some(gameState) : Option.none(),
-    Option.andThen(gs => play.value instanceof Single ? Option.some(gs) : Option.none()),
-    Option.andThen(gs => Option.match(gs.lead, { 
-        onSome: lead => lead.value instanceof Single ? Option.some(gs) : Option.none(), 
-        onNone: () => Option.some(gs) 
-    })),
-    Option.andThen(gs => Card.isSame([Card.getBiggest(getPlayerBySeat(gs.current)(gs).hands), play.cards[0]]) ? Option.none() : Option.some(gs)),
-    Option.andThen(gs => ({ ...gs, suspectedAssistance: true })),
-    Option.getOrElse(() => ({ ...gameState, suspectedAssistance: false }))
-)
-
-const failIfCurrentPlayCannotBeatLead = (play: Play) => (gameState: GameState): Effect.Effect<GameState, GameLogicError> => pipe(
-    gameState.lead,
-    lead => Option.match(lead, { 
-        onNone: () => Effect.succeed(gameState), 
-        onSome: lead => play.canBeat(lead) ? Effect.succeed(gameState) : Effect.fail(new GameLogicError("Current play cannot beat the lead."))
-    })
-    
-)
-
-export const makeMove = (move: Move) => (gameState: GameState): Effect.Effect<GameState, GameLogicError> => {
-    
-}
-
-const makePass = (pass: Pass) => (gameState: GameState): Effect.Effect<GameState, GameLogicError> => pipe(
-    Effect.succeed(gameState),
-    Effect.andThen(failIfLeadNotExists),
-    Effect.andThen(checkIfPassSuspectAssistance(pass)),
-    Effect.andThen(resetLeadIfPassingToLead(pass)),
-    Effect.andThen(assignCurrentToNextPlayer),
-)
-
-const makePlay = (play: Play) => (gameState: GameState): Effect.Effect<GameState, GameLogicError> => {
+const makePass = (pass: Pass) => (gameState: GameState): Effect.Effect<GameState, GameLogicError> => {
     const result = pipe(
         Effect.succeed(gameState),
-        Effect.andThen(failIfCurrentPlayCannotBeatLead(play)),
-        Effect.andThen(checkIfPlaySuspectAssistance(play))
-        // isLeadExists(gameState),
-        // isLeadExists => isLeadExists ? pipe(play.canBeat(Option.getOrThrow(gameState.lead)), canBeat => canBeat ? gameState : Effect.fail(new GameLogicError("Current play cannot beat the lead."))) : gameState,
+        Effect.flatMap(validateLeadExists),
 
     )
 }
+
+/*
+make a pass:
+
+// validataion
+if lead not exists, fail.
+
+// mutation
+update suspect assistance.
+if pass to lead, lead resets.
+update current seat + 1.
+
+*/
